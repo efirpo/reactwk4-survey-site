041c690b131b566f2d8ff1b46186940caf134505
Ethan Firpo <suzannedeakins@Ethan-Firpo-CyberDeck.local>
2020-07-13 16:26:31 -0700

    fix bugs on show detail page

--- src/components/SurveyDetail.js
+++ src/components/SurveyDetail.js
@@ -3,14 +3,14 @@ import React from 'react';
 function SurveyDetail(props) {
   return (
     <React.Fragment>
-      <h1>{props.name}</h1>
-      <p><em>{props.title}</em>, {props.author}</p>
+      <h1>{props.survey.name}</h1>
+      <p><em>{props.survey.title}</em>, {props.survey.author}</p>
       <ul>Survey Questions:
-        <li>{props.question1}</li>
-        <li>{props.question2}</li>
-        <li>{props.question3}</li>
-        <li>{props.question4}</li>
-        <li>{props.question5}</li>
+        <li>{props.survey.question1}</li>
+        <li>{props.survey.question2}</li>
+        <li>{props.survey.question3}</li>
+        <li>{props.survey.question4}</li>
+        <li>{props.survey.question5}</li>
       </ul>
     </React.Fragment>
   )
